e98c539bef3f53c54f1d826de96cbc9ef3836ee6
yueqianz <yueqianz@uw.edu>
2021-02-16 14:16:25 +0800

    sample code

--- src/d3Demo.js
+++ src/d3Demo.js
@@ -11,14 +11,16 @@ var margin = { top: 50, right: 35, bottom: 50, left: 50 },
 w = 650 - (margin.left + margin.right),
 h = 520 - (margin.top + margin.bottom);
 var legendSpace = 130;
+console.log(margin);
 
 // preparation for our x/y axis
 var y = d3.scaleLinear()
           .range([h, 0]);
 var x = d3.scaleTime()
-          .range([ 0, w]);
+          .range([0, w]);
 var yAxis = d3.axisLeft(y);
-var xAxis = d3.axisBottom(x).tickFormat(d3.timeFormat("%b"));   // %b: abbreviated Month format (Mon, Jun..)
+var xAxis = d3.axisBottom(x)
+  .tickFormat(d3.timeFormat("%b"));   // %b: abbreviated Month format (Mon, Jun..)
 
 var citySet = [];
 
@@ -34,52 +36,73 @@ d3.csv(sunshineData).then(function(data) {
 });
 
 function drawLineD3() {
-  colorSet = d3.scaleOrdinal().domain(citySet)
-                .range(d3.schemeSet2);
-  x.domain(d3.extent(sunshineArray, function(d) { return d3.timeParse("%b")(d.month); }));
-  y.domain(d3.extent(sunshineArray, function(d) { return parseFloat(d.sunshine); }));
+  colorSet = d3.scaleOrdinal()
+               .domain(citySet)
+               .range(d3.schemeSet2);
+  x.domain(d3.extent(sunshineArray, d => d3.timeParse("%b")(d.month)));
+  y.domain(d3.extent(sunshineArray, d => parseFloat(d.sunshine)));
 
   // create our svg
-  line_svg = d3.select('#d3-demo').append('svg')
-  .attr("id", "line-chart")
-  .attr("width", w + margin.left + margin.right + legendSpace)
-  .attr("height", h + margin.top + margin.bottom)
-  .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+  line_svg = d3.select('#d3-demo')
+              .append('svg')
+                .attr("id", "line-chart")
+                .attr("width", w + margin.left + margin.right + legendSpace)
+                .attr("height", h + margin.top + margin.bottom)
+              .append('g')
+                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
   // append x axis to svg
   line_svg.append("g")
-  .attr("transform", "translate(0," + h + ")")
-  .attr("class","myXaxis")
-  .call(xAxis);
+            .attr("transform", "translate(0," + h + ")")
+            .attr("class","myXaxis")
+            .call(xAxis);
 
   // append y axis to svg
   line_svg.append("g")
-  .attr("class","myYaxis")
-  .call(yAxis);
+            .attr("class","myYaxis")
+            .call(yAxis);
 
-// make a line for each city
+  // create a group to store lines for our line chart
+  var path = line_svg.append('g')
+                        .attr("id","paths-group");
+
+  var line = d3.line()
+              .x(d => x(d3.timeParse("%b")(d.month)))
+              .y(d => y(parseFloat(d.sunshine)));  
+
+  // make a line for each city
   citySet.forEach(function(d) {
-      var currentCity = sunshineArray.filter(function(e) {return e.city === d;});
-
-      var line = d3.line()
-      .x(function(d){return x(d3.timeParse("%b")(d.month));})
-      .y(function(d){return y(parseFloat(d.sunshine));});
-
-      var path = line_svg.append("path")
-        .datum(currentCity)
-        .attr("class", "lines")
-        .attr('d', line)
-        .style("stroke-width", 2.5)
-        .style("fill", "none")
-        .attr("stroke", colorSet(d))
+      var currentCity = sunshineArray.filter(e => e.city === d);
+      path.append("path")
+            .datum(currentCity)
+            .attr("class", "lines")
+            .attr('d', line)
+            .style("stroke-width", 2.5)
+            .style("fill", "none")
+            .attr("stroke", colorSet(d))
   });
 
   // add legend
-  for (let i = 0; i < citySet.length; i++) {
-    line_svg.append("rect").attr("class","legends").attr("x",600).attr("y",25+30*i).attr("width", 10).attr("height", 10).style("fill", colorSet(citySet[i]));
-    line_svg.append("text").attr("class","legends").attr("x", 620).attr("y", 30+30*i).text(citySet[i]).style("font-size", "15px").attr("alignment-baseline","middle")
-  }
+  var legend = line_svg.append('g')
+                        .attr("id","legend-group");
+
+  legend.selectAll("rect").data(citySet)
+      .join("rect")
+        .attr("class","legends")
+        .attr("x",600)
+        .attr("y", d => 25+30*(citySet.indexOf(d)))
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", d => colorSet(d));
+
+  legend.selectAll("text").data(citySet)
+      .join("text")
+        .attr("class","legends")
+        .attr("x", 620)
+        .attr("y", d => 30+30*(citySet.indexOf(d)))
+        .text(d => d)
+        .style("font-size", "15px")
+        .attr("alignment-baseline","middle");
 }
 
 
