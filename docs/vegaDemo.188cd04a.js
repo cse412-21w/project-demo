cbfab6aedc59266c4ce55e981f7f289daa185794
Sonya Lao <lao.sonya@gmail.com>
2021-02-15 22:11:05 -0800

    updated docs

--- docs/vegaDemo.188cd04a.js
+++ docs/vegaDemo.188cd04a.js
@@ -118,8 +118,8 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"ZQwg":[function(require,module,exports) {
-module.exports = "https://cse412.github.io/project-demo/sunshine.71fb7d0b.csv";
-},{}],"Edg9":[function(require,module,exports) {
+module.exports = "https://cse412-21w.github.io/project-demo/sunshine.71fb7d0b.csv";
+},{}],"CsaW":[function(require,module,exports) {
 "use strict";
 
 var _sunshine = _interopRequireDefault(require("../static/sunshine.csv"));
@@ -131,29 +131,26 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 // With strict mode, you can not, for example, use undeclared variables
 
 
-var line_svg; // used for svg later
-
-var colorSet; // used for color scheme later
-
 var sunshineArray = []; // used to store data later
-// preparation for our svg
 
-var margin = {
-  top: 50,
-  right: 35,
-  bottom: 50,
-  left: 50
-},
-    w = 650 - (margin.left + margin.right),
-    h = 520 - (margin.top + margin.bottom);
-var legendSpace = 130; // preparation for our x/y axis
-
-var y = d3.scaleLinear().range([h, 0]);
-var x = d3.scaleTime().range([0, w]);
-var yAxis = d3.axisLeft(y);
-var xAxis = d3.axisBottom(x).tickFormat(d3.timeFormat("%b")); // %b: abbreviated Month format (Mon, Jun..)
-
-var citySet = []; // once finish processing data, make a graph!
+var citySet = [];
+var options = {
+  config: {// Vega-Lite default configuration
+  },
+  init: function init(view) {
+    // initialize tooltip handler
+    view.tooltip(new vegaTooltip.Handler().call);
+  },
+  view: {
+    // view constructor options
+    // remove the loader if you don't want to default to vega-datasets!
+    //   loader: vega.loader({
+    //     baseURL: "",
+    //   }),
+    renderer: "canvas"
+  }
+};
+vl.register(vega, vegaLite, options); // Again, We use d3.csv() to process data
 
 d3.csv(_sunshine.default).then(function (data) {
   data.forEach(function (d) {
@@ -163,40 +160,20 @@ d3.csv(_sunshine.default).then(function (data) {
       citySet.push(d.city);
     }
   });
-  drawLineD3();
+  drawBarVegaLite();
 });
 
-function drawLineD3() {
-  colorSet = d3.scaleOrdinal().domain(citySet).range(d3.schemeSet2);
-  x.domain(d3.extent(sunshineArray, function (d) {
-    return d3.timeParse("%b")(d.month);
-  }));
-  y.domain(d3.extent(sunshineArray, function (d) {
-    return parseFloat(d.sunshine);
-  })); // create our svg
-
-  line_svg = d3.select('#d3-demo').append('svg').attr("id", "line-chart").attr("width", w + margin.left + margin.right + legendSpace).attr("height", h + margin.top + margin.bottom).append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')'); // append x axis to svg
-
-  line_svg.append("g").attr("transform", "translate(0," + h + ")").attr("class", "myXaxis").call(xAxis); // append y axis to svg
-
-  line_svg.append("g").attr("class", "myYaxis").call(yAxis); // make a line for each city
-
-  citySet.forEach(function (d) {
-    var currentCity = sunshineArray.filter(function (e) {
-      return e.city === d;
-    });
-    var line = d3.line().x(function (d) {
-      return x(d3.timeParse("%b")(d.month));
-    }).y(function (d) {
-      return y(parseFloat(d.sunshine));
-    });
-    var path = line_svg.append("path").datum(currentCity).attr("class", "lines").attr('d', line).style("stroke-width", 2.5).style("fill", "none").attr("stroke", colorSet(d));
-  }); // add legend
-
-  for (var i = 0; i < citySet.length; i++) {
-    line_svg.append("rect").attr("class", "legends").attr("x", 600).attr("y", 25 + 30 * i).attr("width", 10).attr("height", 10).style("fill", colorSet(citySet[i]));
-    line_svg.append("text").attr("class", "legends").attr("x", 620).attr("y", 30 + 30 * i).text(citySet[i]).style("font-size", "15px").attr("alignment-baseline", "middle");
-  }
+function drawBarVegaLite() {
+  // var sunshine = add_data(vl, sunshine.csv, format_type = NULL);
+  // your visualization goes here
+  vl.markBar({
+    filled: true,
+    color: 'teal'
+  }).data(sunshineArray).encode(vl.x().fieldN('month').sort('none'), vl.y().fieldQ('sunshine'), vl.tooltip(['sunshine'])).width(450).height(450).render().then(function (viewElement) {
+    // render returns a promise to a DOM element containing the chart
+    // viewElement.value contains the Vega View object instance
+    document.getElementById('view').appendChild(viewElement);
+  });
 }
-},{"../static/sunshine.csv":"ZQwg"}]},{},["Edg9"], null)
-//# sourceMappingURL=https://cse412.github.io/project-demo/d3-demo.3cfb303b.js.map
+},{"../static/sunshine.csv":"ZQwg"}]},{},["CsaW"], null)
+//# sourceMappingURL=https://cse412-21w.github.io/project-demo/vegaDemo.188cd04a.js.map
